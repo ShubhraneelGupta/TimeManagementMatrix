c6ed3cddc51db256d0b0c1bfd107656f21e667a3
Shubhraneel Gupta <btech10583.22@bitmesra.ac.in>
2024-10-12 19:52:06 +0530

    Update Matrix.jsx

--- src/Matrix.jsx
+++ src/Matrix.jsx
@@ -1,7 +1,7 @@
-import {useState, useRef, useEffect} from 'react'
-import './Matrix.scss'
+import { useState, useRef, useEffect } from 'react';
+import './Matrix.scss';
 
-const Box = ({storageKey}) => {
+const Box = ({ storageKey }) => {
     const [items, setItems] = useState(() => {
         const savedItems = localStorage.getItem(storageKey);
         return savedItems ? JSON.parse(savedItems) : [];
@@ -11,60 +11,73 @@ const Box = ({storageKey}) => {
         localStorage.setItem(storageKey, JSON.stringify(items));
     }, [items, storageKey]);
 
-    const inputRef = useRef()
+    const inputRef = useRef();
 
     const handleAdd = () => {
         const input = inputRef.current.value;
-        if(input){
-            setItems([...items, inputRef.current.value])
+        if (input) {
+            setItems([...items, { text: input }]); // Store text as object
         }
-        inputRef.current.value = ''
-    }
+        inputRef.current.value = '';
+    };
 
     const handleRemove = (index) => {
-        setItems(items.filter((val, id) => id != index))
-    }
+        setItems(items.filter((_, id) => id !== index));
+    };
 
-    return <div className="box">
-        <div className="header">
-            <input 
-            ref={inputRef}
-            onKeyDown={(e) => {if(e.key === 'Enter') return handleAdd()}}
-            type="text" />
-
-            <button
-            className='add-button'
-            onClick={handleAdd}
-            >+</button>
+    return (
+        <div className="box">
+            <div className="header">
+                <input
+                    ref={inputRef}
+                    onKeyDown={(e) => {
+                        if (e.key === 'Enter') return handleAdd();
+                    }}
+                    type="text"
+                />
+                <button className="add-button" onClick={handleAdd}>
+                    +
+                </button>
+            </div>
+            <br />
+            <ul>
+                {items.map((val, index) => (
+                    <li key={index}>
+                        {val.text}
+                        <button className="remove-button" onClick={() => handleRemove(index)}>
+                            -
+                        </button>
+                    </li>
+                ))}
+            </ul>
         </div>
-        <br />
-        {items.map((val, index) => {
-            return <li key={index}>{val}<button
-            className='remove-button'
-            onClick={() => handleRemove(index)}
-            >-</button></li>
-        })}
-    </div>
-}
+    );
+};
 
 const Matrix = () => {
-    return <table className="matrix-table">
-        <tr>
-            <td></td>
-            <td><h1>Urgent</h1></td>
-            <td><h1>Not Urgent</h1></td>
-        </tr>
-        <tr>
-            <td className='side'><h1>Important</h1></td>
-            <td><Box storageKey={'box1'}/></td>
-            <td><Box storageKey={'box2'}/></td>
-        </tr>
-        <tr>
-            <td className='side'><h1>Not Important</h1></td>
-            <td><Box storageKey={'box3'}/></td>
-            <td><Box storageKey={'box4'}/></td>
-        </tr>
-    </table>
-}
+    return (
+        <table className="matrix-table">
+            <thead>
+                <tr>
+                    <td></td>
+                    <td><h1>Urgent</h1></td>
+                    <td><h1>Not Urgent</h1></td>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td className="side"><h1>Important</h1></td>
+                    <td><Box storageKey={'box1'} /></td>
+                    <td><Box storageKey={'box2'} /></td>
+                </tr>
+                <tr>
+                    <td className="side"><h1>Not Important</h1></td>
+                    <td><Box storageKey={'box3'} /></td>
+                    <td><Box storageKey={'box4'} /></td>
+                </tr>
+            </tbody>
+        </table>
+    );
+};
 
 export default Matrix;
